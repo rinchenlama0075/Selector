c69faf1f19f12ec851da08edad0ddc2e520c4d69
Rinchen Lama <rinchenlama0075@gmail.com>
2019-07-17 16:13:52 -0400

    Updates

---
+++ precache-manifest.3438aff31425958c2858a0f02e6eb220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68086ea60a96c219edd59fded28db8fb",
+    "url": "/Selector/index.html"
+  },
+  {
+    "revision": "a457218f4d81d904c0b7",
+    "url": "/Selector/static/css/main.023b2a60.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Selector/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a457218f4d81d904c0b7",
+    "url": "/Selector/static/js/main.2656dc41.chunk.js"
+  },
+  {
+    "revision": "828673a301b0d70ac008",
+    "url": "/Selector/static/js/runtime~main.0678f3e5.js"
+  }
+]);
